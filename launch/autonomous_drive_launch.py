dcbac037ae4a77b23a54537db582a616c210c94a
Yannick Wagener <yannick.wagener@ppimedia.de>
2024-09-18 15:04:57 +0200

    added initial content for integrational launch file

--- launch/autonomous_drive_launch.py
+++ launch/autonomous_drive_launch.py
@@ -1,1 +1,33 @@
-#todo implement
+from launch_ros.actions import Node
+
+from launch import LaunchDescription
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        # Linienverfolgungs-Node
+        Node(
+            package='robotik_projekt',
+            executable='line_following',
+            output='screen',
+            emulate_tty=True,
+            arguments=['__log_level:=debug']
+        ),
+
+        # Hindernisvermeidungs-Node mit Laserscanner
+        Node(
+            package='robotik_projekt',
+            executable='drive_with_laserscanner',
+            output='screen',
+            emulate_tty=True,
+            arguments=['__log_level:=debug'],
+        ),
+        # State Machine Kontrollnode
+        Node(
+            package='robotik_projekt',
+            executable='state_machine',
+            output='screen',
+            emulate_tty=True,
+            arguments=['__log_level:=debug'],
+        )
+    ])
